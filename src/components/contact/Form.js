1b8ea0dc046a58161f3e3cd253a67d57d0a4ae9c
Roza001 <roza_1@op.pl>
2021-05-15 01:16:49 +0200

    recaptcha fix

--- src/components/contact/Form.js
+++ src/components/contact/Form.js
@@ -1,93 +1,174 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Button,  Box } from "@material-ui/core";
+import { Typography, Button, Box } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import emailjs from "emailjs-com";
+import ReCAPTCHA from "react-google-recaptcha";
 import "./Form.scss";
+
+const initialState = {
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "name":
+      return { ...state, name: action.value };
+    case "email":
+      return { ...state, email: action.value };
+    case "subject":
+      return { ...state, subject: action.value };
+    case "message":
+      return { ...state, message: action.value };
+    default:
+      throw new Error();
+  }
+}
 
 const useStyles = makeStyles({
   form: {
     padding: "2em",
-    marginLeft: "5em"
+    marginLeft: "5em",
   },
   button: {
     marginTop: "1rem",
     color: "tan",
     borderColor: "tan",
-    width:"80%",
-    "&:hover":{
+    width: "80%",
+    "&:hover": {
       transform: "scale(1.005)",
-      backgroundColor: "rgba(20,20,20,0.9)"
-    }
-  },  
+      backgroundColor: "rgba(20,20,20,0.9)",
+    },
+  },
 });
-
-function sendEmail(e) {
-  console.log("sending");
-  console.log(e.target);
-  e.preventDefault();
-  emailjs.sendForm('service_eemeyxn', 'template_portfolio', e.target, 'user_D7s7BN3PxuGJ1RoDO0gW0')
-  .then((result) => {
-  console.log(result.text);
-  }, (error) => {
-  console.log(error.text);
-  });
-  e.target.reset();
-}
 
 const Form = () => {
   const classes = useStyles();
-  return (
-  <Box component="div" alignItems="center" justifyContent="center" >
+  const [showFormErr, setShowFormErr] = useState(false);
+  const [formState, dispatch] = useReducer(reducer, initialState);
+  const [formSubmitted, setFormSubmitted] = useState({
+    title: "",
+    paragraph: "",
+  });
+  const [showCaptcha, setShowCaptcha] = useState(false);
+  const { name, email, message } = formState;
+
+  const submitFormAndShowCaptcha = (e) => {
+    e.preventDefault();
+    setShowCaptcha(true);
+  };
+
+  const sendEmail = (captchaValue) => {
+    if (name === "" || email === "" || message === "") {
+      setShowFormErr(true);
+      return;
+    }
+
+    const params = {
+      ...formState,
+      "g-recaptcha-response": captchaValue,
+    };
+
+    setFormSubmitted({ title: "Sending message...", paragraph: "" });
+    emailjs
+      .send(
+        "service_eemeyxn",
+        "template_portfolio",
+        params,
+        "user_D7s7BN3PxuGJ1RoDO0gW0"
+      )
+      .then(
+        ({ status }) => {
+          if (status === 200) {
+            setFormSubmitted({
+              title: "Message has been sent",
+              paragraph: "I will be in contact with you soon.",
+            });
+          } else {
+            setFormSubmitted({
+              title: "Error sending message, try again later",
+              paragraph: "",
+            });
+          }
+        },
+        (err) => {
+          console.log(err);
+          setFormSubmitted({
+            title: "Error sending message, try again later",
+            paragraph: "",
+          });
+        }
+      );
+  };
+
+  return formSubmitted.title === "" ? (
+    <Box component="div" alignItems="center" justifyContent="center">
       <Typography
         variant="h4"
         style={{
           color: "#ECCA79D6",
           textAlign: "start",
           paddingLeft: "3em",
-          fontSize: "3em", 
+          fontSize: "3em",
         }}
       >
         Contact Me
       </Typography>
-      <form onSubmit={sendEmail} className={classes.form}>
-        <div className="col-xs-10">
-          <div className="styled-input wide">
-            <input type="text" required name="name" id="name" />
-            <label>Name</label>
-          </div>          
-        </div>
+      {!showCaptcha ? (
+        <form onSubmit={submitFormAndShowCaptcha} className={classes.form}>
+          <div className="col-xs-10">
+            <div className="styled-input wide">
+              <input type="text" required name="name" id="name" onChange={(e) => dispatch({ type: 'name', value: e.target.value })}/>
+              <label>Name</label>
+            </div>
+          </div>
 
-        <div className="col-xs-10" >
-          <div className="styled-input wide">
-            <input type="text" required name="email" id="email" />
-            <label>Email</label>
+          <div className="col-xs-10">
+            <div className="styled-input wide">
+              <input type="text" required name="email" id="email"  onChange={(e) => dispatch({ type: 'email', value: e.target.value })}/>
+              <label>Email</label>
+            </div>
           </div>
-        </div>        
-        <div className="col-xs-10">
-          <div className="styled-input wide" >
-            <input type="text" required name="subject" id="subject" />
-            <label>Subject</label>
+          <div className="col-xs-10">
+            <div className="styled-input wide">
+              <input type="text" required name="subject" id="subject"  onChange={(e) => dispatch({ type: 'subject', value: e.target.value })}/>
+              <label>Subject</label>
+            </div>
           </div>
-        </div>
-        <div className="col-xs-10">
-          <div className="styled-input wide">
-            <textarea required name="message" id="message"></textarea>
-            <label>Message</label>
+          <div className="col-xs-10">
+            <div className="styled-input wide">
+              <textarea required name="message" id="message" onChange={(e) => dispatch({ type: 'message', value: e.target.value })}></textarea>
+              <label>Message</label>
+            </div>
           </div>
-        </div> 
-       
-        <Button
-          className={classes.button}
-          variant="outlined"
-          fullWidth={true}
-          endIcon={<SendIcon />}
-          type="submit"
-        >
-          Send Email
-        </Button>
-      </form>
+          {showFormErr ? <p className="sm:mr-4 text-red-400">Please fill in all three input boxes to send a message</p> : null}
+          <Button
+            className={classes.button}
+            variant="outlined"
+            fullWidth={true}
+            endIcon={<SendIcon />}
+            type="submit"
+          >
+            Send Email
+          </Button>
+        </form>
+      ) : (
+        <ReCAPTCHA
+          sitekey="6LcicNUaAAAAAG8T5H0Z7E9uDbl3SfJtUwm79J91"
+          onChange={sendEmail}
+        />
+      )}
     </Box>
+  ) : (
+    <div className="flex flex-col items-center">
+      <h3 className="text-lato text-2xl font-light text-white">
+        {formSubmitted.title}
+      </h3>
+      <p>{formSubmitted.paragraph}</p>
+    </div>
   );
 };
 
